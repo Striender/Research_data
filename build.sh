feafff90862ca17325302ed6cb7202857bb54a1b
Striender <striender1809@gmail.com>
2026-01-25 18:59:18 +0000

    create a custom build script for building all the combination and also a specific combination too

--- build.sh
+++ build.sh
@@ -1,6 +1,30 @@
-if [ $# -ne 4 ]; then
-    echo "Usage: $0 <L1 Prefetcher> <L2 Prefetcher> <L2 Replacement Policy> <LLC Replacement Policy> "
-    exit 1
-fi
+#!/bin/bash
 
-./build_champsim.sh bimodal no $1 $2 no no no no lru lru lru $3 $4 lru lru lru 1 no 
+
+repl_policies=("lru" "srrip" "drrip" "hawkeye" "ship" "ship++" "mockingjay" "lru" "srrip" "drrip" "hawkeye" "ship" "ship++" "mockingjay")
+
+
+if [ "$#" -eq 4 ]; then
+    ./build_champsim.sh hashed_perceptron no $1 $2 no no no no lru lru lru $3 $4 lru lru lru 1 no
+
+elif [ "$#" -eq 2 ] || [ "$#" -eq 3 ] ; then
+    N=${3:-1}
+    for (( j=N; j<=14; j++ )); do
+        if (( j < 8 )); then
+            ./build_champsim.sh hashed_perceptron no $1 $2 no no no no lru lru lru lru "${repl_policies[$((j-1))]}" lru lru lru 1 no
+        else
+            ./build_champsim.sh hashed_perceptron no $1 $2 no no no no lru lru lru srrip "${repl_policies[$((j-1))]}" lru lru lru 1 no
+        fi
+    done
+
+else
+    echo "Invalid number of input"
+    echo "U can Either give 4 inputs or 2 inputs"
+    echo
+    echo "Usage of 4 inputs: <L1 Prefetcher> <L2 Prefetcher> <L2 Replacement Policy> <LLC Replacement Policy> "
+    echo "./build.sh berti spp lru srrip"
+    echo
+    echo "Usage of 2 inputs:  <L1 Prefetcher> <L2 Prefetcher> "
+    echo "./build.sh berti spp "
+    echo
+fi
