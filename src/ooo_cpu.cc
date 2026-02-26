54feb9c1b6e11a0270a92b0f9e8a0ed52629e090
Striender <striender1809@gmail.com>
2026-02-26 11:29:48 +0000

    added the more parameters to the last_branch_result

--- src/ooo_cpu.cc
+++ src/ooo_cpu.cc
@@ -190,8 +190,9 @@ void O3_CPU::read_from_trace()
 					}
 				}
 
-			last_branch_result(IFETCH_BUFFER.entry[ifetch_buffer_index].ip, IFETCH_BUFFER.entry[ifetch_buffer_index].branch_taken);
-			}
+            last_branch_result(IFETCH_BUFFER.entry[ifetch_buffer_index].ip, IFETCH_BUFFER.entry[ifetch_buffer_index].branch_target, IFETCH_BUFFER.entry[ifetch_buffer_index].branch_taken, IFETCH_BUFFER.entry[ifetch_buffer_index].branch_type);
+	
+        }
 
 
 				
@@ -485,7 +486,7 @@ void O3_CPU::read_from_trace()
 				}
 			}
 
-			last_branch_result(IFETCH_BUFFER.entry[ifetch_buffer_index].ip, IFETCH_BUFFER.entry[ifetch_buffer_index].branch_taken);
+            last_branch_result(IFETCH_BUFFER.entry[ifetch_buffer_index].ip, IFETCH_BUFFER.entry[ifetch_buffer_index].branch_target, IFETCH_BUFFER.entry[ifetch_buffer_index].branch_taken, IFETCH_BUFFER.entry[ifetch_buffer_index].branch_type);
 			}
 
 
