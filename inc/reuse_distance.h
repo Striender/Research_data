21d8333e0f9f89a148e97abfa2f60394ec200119
Striender <striender1809@gmail.com>
2026-07-07 08:49:57 +0000

    Added the functionality to calculate reuse distance for L2

--- inc/reuse_distance.h
+++ inc/reuse_distance.h
@@ -4,6 +4,7 @@
 #include <cstdint>
 
 void reuse_distance_access(uint32_t cpu, uint64_t addr, uint8_t type);
+void reuse_distance_l2c_access(uint32_t cpu, uint64_t addr, uint8_t type);
 void reuse_distance_clear();
 void reuse_distance_final_stats();
 
--- src/reuse_distance.cc
+++ src/reuse_distance.cc
@@ -1,162 +1,156 @@
 #include "reuse_distance.h"
-#include "memory_class.h"
 #include "cache.h"
+#include "memory_class.h"
 
 #include <cstdint>
 #include <iostream>
-#include <map>
+#include <list>
 #include <unordered_map>
-#include <utility>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 
-using __gnu_pbds::null_type;
-using __gnu_pbds::rb_tree_tag;
-using __gnu_pbds::tree;
-using __gnu_pbds::tree_order_statistics_node_update;
+static const int64_t COLD_MISS_RD = -1;
+static const uint32_t MID_REUSE_LIMIT = 32;
 
 class ReuseDistanceTracker {
   public:
     uint64_t access_count = 0;
     uint64_t cold_count = 0;
 
-    int64_t access(uint64_t line_addr)
+    int64_t access(uint64_t line_addr, uint32_t num_set, uint32_t num_way)
     {
-        uint32_t set = line_addr % L1D_SET;
+        uint32_t set = line_addr % num_set;
         SetState &set_state = set_states[set];
 
         access_count++;
-        set_state.access_count++;
 
-        auto it = set_state.last_access_time.find(line_addr);
-        if (it == set_state.last_access_time.end()) {
+        auto it = set_state.line_position.find(line_addr);
+
+        // First time this line is accessed: cold miss
+        if (it == set_state.line_position.end()) {
             cold_count++;
-            set_state.last_access_time[line_addr] = set_state.access_count;
-            set_state.access_order.insert(std::make_pair(set_state.access_count, line_addr));
-            reuse_distance_frequency[line_addr][-1]++;
-            return -1;
+
+            set_state.lru_stack.push_front(line_addr);
+            set_state.line_position[line_addr] = set_state.lru_stack.begin();
+            return COLD_MISS_RD;
         }
 
-        uint64_t old_time = it->second;
-        uint64_t older_or_equal = set_state.access_order.order_of_key(std::make_pair(old_time, UINT64_MAX));
-        int64_t rd = static_cast<int64_t>(set_state.access_order.size() - older_or_equal);
+        int64_t rd = get_reuse_distance(set_state, line_addr);
+        add_bin(rd, num_way);
 
-        reuse_distance_frequency[line_addr][rd]++;
-        set_state.access_order.erase(std::make_pair(old_time, line_addr));
-
-        it->second = set_state.access_count;
-        set_state.access_order.insert(std::make_pair(set_state.access_count, line_addr));
+        set_state.lru_stack.splice(set_state.lru_stack.begin(),
+                                   set_state.lru_stack,
+                                   it->second);
+        it->second = set_state.lru_stack.begin();
 
         return rd;
     }
 
-    void print_stats(uint32_t cpu) const
-    {
-        std::cout << "CPU " << cpu << " reuse accesses: " << access_count
-                  << " cold: " << cold_count << std::endl;
-        std::cout << "CPU " << cpu << " reuse distance frequency table" << std::endl;
-
-        for (const auto &addr_entry : reuse_distance_frequency) {
-            const auto &distance_counts = addr_entry.second;
-            bool was_reused = false;
-
-            for (const auto &rd_entry : distance_counts) {
-                if (rd_entry.first != -1) {
-                    was_reused = true;
-                    break;
-                }
-            }
-
-            if (!was_reused)
-                continue;
-
-            uint64_t line_addr = addr_entry.first;
-            uint64_t byte_addr = line_addr << LOG2_BLOCK_SIZE;
-
-            std::cout << "Address: 0x" << std::hex << byte_addr << std::dec << std::endl;
-            std::cout << "reuse_distance,frequency" << std::endl;
-
-            for (const auto &rd_entry : distance_counts) {
-                if (rd_entry.first == -1)
-                    continue;
-
-                std::cout << rd_entry.first << "," << rd_entry.second << std::endl;
-            }
-        }
-    }
-
     void add_bin_counts(uint64_t bins[3]) const
     {
-        for (const auto &addr_entry : reuse_distance_frequency) {
-            for (const auto &rd_entry : addr_entry.second) {
-                int64_t rd = rd_entry.first;
-
-                if (rd < 0)
-                    continue;
-                else if (rd < L1D_WAY)
-                    bins[0] += rd_entry.second;
-                else if (rd < 32)
-                    bins[1] += rd_entry.second;
-                else
-                    bins[2] += rd_entry.second;
-            }
-        }
+        bins[0] += bin_counts[0];
+        bins[1] += bin_counts[1];
+        bins[2] += bin_counts[2];
     }
 
     void clear()
     {
         access_count = 0;
         cold_count = 0;
+        bin_counts[0] = 0;
+        bin_counts[1] = 0;
+        bin_counts[2] = 0;
         set_states.clear();
-        reuse_distance_frequency.clear();
     }
 
   private:
-    using AccessKey = std::pair<uint64_t, uint64_t>;
-    using OrderedAccessSet = tree<AccessKey, null_type, std::less<AccessKey>,
-                                  rb_tree_tag, tree_order_statistics_node_update>;
-
     struct SetState {
-        uint64_t access_count = 0;
-        std::unordered_map<uint64_t, uint64_t> last_access_time;
-        OrderedAccessSet access_order;
+        std::list<uint64_t> lru_stack;
+        std::unordered_map<uint64_t, std::list<uint64_t>::iterator> line_position;
     };
 
+    int64_t get_reuse_distance(const SetState &set_state, uint64_t line_addr) const
+    {
+        int64_t rd = 0;
+
+        for (auto it = set_state.lru_stack.begin();
+             it != set_state.lru_stack.end() && rd < MID_REUSE_LIMIT;
+             ++it, ++rd) {
+            if (*it == line_addr)
+                return rd;
+        }
+
+        return MID_REUSE_LIMIT;
+    }
+
+    void add_bin(int64_t rd, uint32_t num_way)
+    {
+        if (rd < 0)
+            return;
+
+        if (rd < num_way)
+            bin_counts[0]++;
+        else if (rd < MID_REUSE_LIMIT)
+            bin_counts[1]++;
+        else
+            bin_counts[2]++;
+    }
+
+    // set index -> state of that set
     std::unordered_map<uint32_t, SetState> set_states;
-    std::map<uint64_t, std::map<int64_t, uint64_t>> reuse_distance_frequency;
+    uint64_t bin_counts[3] = {0, 0, 0};
 };
 
-static ReuseDistanceTracker tracker[NUM_CPUS];
+static ReuseDistanceTracker l1d_tracker[NUM_CPUS];
+static ReuseDistanceTracker l2c_tracker[NUM_CPUS];
 
-void reuse_distance_access(uint32_t cpu, uint64_t addr, uint8_t type)
+static void record_access(ReuseDistanceTracker tracker[], uint32_t cpu, uint64_t addr, uint8_t type, uint32_t num_set, uint32_t num_way)
 {
     if (type == PREFETCH)
         return;
 
     uint64_t line_addr = addr >> LOG2_BLOCK_SIZE;
-    tracker[cpu].access(line_addr);
+    tracker[cpu].access(line_addr, num_set, num_way);
+}
+
+static void print_bins(const char *name,
+                       const char *way_name,
+                       const uint64_t bins[3])
+{
+    std::cout << "==============================================" << std::endl;
+    std::cout << name << " reuse distance bin frequency table" << std::endl;
+    std::cout << "reuse_distance_bin,frequency" << std::endl;
+    std::cout << "short[0-" << way_name << ")," << bins[0] << std::endl;
+    std::cout << "mid[" << way_name << "-32)," << bins[1] << std::endl;
+    std::cout << "long[32+]," << bins[2] << std::endl;
+}
+
+void reuse_distance_access(uint32_t cpu, uint64_t addr, uint8_t type)
+{
+    record_access(l1d_tracker, cpu, addr, type, L1D_SET, L1D_WAY);
+}
+
+void reuse_distance_l2c_access(uint32_t cpu, uint64_t addr, uint8_t type)
+{
+    record_access(l2c_tracker, cpu, addr, type, L2C_SET, L2C_WAY);
 }
 
 void reuse_distance_clear()
 {
     for (uint32_t cpu = 0; cpu < NUM_CPUS; cpu++) {
-        tracker[cpu].clear();
+        l1d_tracker[cpu].clear();
+        l2c_tracker[cpu].clear();
     }
 }
 
 void reuse_distance_final_stats()
 {
-    uint64_t bins[3] = {0, 0, 0};
+    uint64_t l1d_bins[3] = {0, 0, 0};
+    uint64_t l2c_bins[3] = {0, 0, 0};
 
     for (uint32_t cpu = 0; cpu < NUM_CPUS; cpu++) {
-        //tracker[cpu].print_stats(cpu);
-        tracker[cpu].add_bin_counts(bins);
+        l1d_tracker[cpu].add_bin_counts(l1d_bins);
+        l2c_tracker[cpu].add_bin_counts(l2c_bins);
     }
 
-    std::cout <<"==============================================" << std::endl;
-    std::cout << "Reuse distance bin frequency table" << std::endl;
-    std::cout << "reuse_distance_bin,frequency" << std::endl;
-    std::cout << "short[0-L1D_WAY)," << bins[0] << std::endl;
-    std::cout << "mid[L1D_WAY-32)," << bins[1] << std::endl;
-    std::cout << "long[32+]," << bins[2] << std::endl;
+    print_bins("L1D", "L1D_WAY", l1d_bins);
+    print_bins("L2C", "L2C_WAY", l2c_bins);
 }
